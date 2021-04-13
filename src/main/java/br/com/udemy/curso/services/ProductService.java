37cc6f32859c4afb792d443e21b6c58ca6173cc8
JamilsonPereira <jamilson.nunesp@hotmail.com>
2021-04-12 23:45:11 -0300

    Products

---
+++ src/main/java/br/com/udemy/curso/services/ProductService.java
@@ -0,0 +1,27 @@
+package br.com.udemy.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.udemy.curso.entities.Product;
+import br.com.udemy.curso.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll(); 
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id); 
+		return obj.get();
+	}
+
+}
